55866fc333ee5d39035206c567d5e832cdc2dc6e
DorineN <dorine-niel@hotmail.fr>
2018-03-12 10:56:06 +0100

    Ajout de tests sur PrimeAnnuelle

--- src/test/java/com/ipiecoles/java/java350/model/CommercialTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/CommercialTest.java
@@ -21,4 +21,43 @@ public class CommercialTest {
         Assertions.assertThat(prime).isEqualTo(500d);
     }
 
+    @Test
+    public void testPrimeAnnuelleIsHigherThan500d() {
+        //GIVEN - Initialisation des données
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(10000.50);
+
+        //WHEN - Exécution/test de la méthode
+        Double prime = commercial.getPrimeAnnuelle();
+
+        //THEN - Vérification par rapport à la sortie de la méthode
+        Assertions.assertThat(prime).isGreaterThan(500d);
+    }
+
+    @Test
+    public void testPrimeAnnuelleIsBelowThan500d() {
+        //GIVEN - Initialisation des données
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(50.50);
+
+        //WHEN - Exécution/test de la méthode
+        Double prime = commercial.getPrimeAnnuelle();
+
+        //THEN - Vérification par rapport à la sortie de la méthode
+        Assertions.assertThat(prime).isEqualTo(500d);
+    }
+
+    @Test
+    public void testPrimeAnnuelleIsPositive() {
+        //GIVEN - Initialisation des données
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(50.50);
+
+        //WHEN - Exécution/test de la méthode
+        Double prime = commercial.getPrimeAnnuelle();
+
+        //THEN - Vérification par rapport à la sortie de la méthode
+        Assertions.assertThat(prime).isNotNegative();
+    }
+
 }
